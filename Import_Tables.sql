ddf8ef47f421f130e747fe27f5126f7178b4b8b3
LorinMader <151132677+LorinMader@users.noreply.github.com>
2024-01-12 10:28:32 +0100

    Update Import_Tables.sql

--- Import_Tables.sql
+++ Import_Tables.sql
@@ -1,21 +1,21 @@
 USE Modul_141;
-INSERT INTO modul_141.type(name) 
+INSERT INTO Modul_141.type(name) 
 SELECT DISTINCT type
 FROM import;
 
-INSERT INTO modul_141.usergroup(`GID`, `Group`)
+INSERT INTO Modul_141.usergroup(`GID`, `Group`)
 SELECT DISTINCT gid, `group`
 FROM import;
 
-INSERT INTO modul_141.systemuser(`UID`, `User`)
+INSERT INTO Modul_141.systemuser(`UID`, `User`)
 SELECT DISTINCT uid, `user`
 FROM import;
 
-INSERT INTO modul_141.data(`digest`, `content`, `size`, `compression`, `Type_ID`)
+INSERT INTO Modul_141.data(`digest`, `content`, `size`, `compression`, `Type_ID`)
 SELECT DISTINCT `digest`, `data`, `size`, `compression`, (SELECT ID FROM modul_141.type where type.name = import.type)
 FROM import;
 
-INSERT INTO modul_141.meta(`path`, `perm`, `time`, `Systemuser_UID`, `USergroup_GID`, `Data_digest`)
+INSERT INTO Modul_141.meta(`path`, `perm`, `time`, `Systemuser_UID`, `USergroup_GID`, `Data_digest`)
 SELECT DISTINCT `path`, `perm`, `time`, `uid`, `gid`, `digest`
 FROM import;
 
