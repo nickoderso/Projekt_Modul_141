39d2f055574e96ce353cc1f6c4ebdd85449990e4
Mike <111377817+Chaeschtli@users.noreply.github.com>
2024-01-12 10:40:55 +0100

    Update Import_Tables.sql
    Update

--- Import_Tables.sql
+++ Import_Tables.sql
@@ -1,22 +1,22 @@
 USE Modul_141;
 INSERT INTO Modul_141.type(name) 
 SELECT DISTINCT type
-FROM import;
+FROM Import;
 
 INSERT INTO Modul_141.usergroup(`GID`, `Group`)
 SELECT DISTINCT gid, `group`
-FROM import;
+FROM Import;
 
 INSERT INTO Modul_141.systemuser(`UID`, `User`)
 SELECT DISTINCT uid, `user`
-FROM import;
+FROM Import;
 
 INSERT INTO Modul_141.data(`digest`, `content`, `size`, `compression`, `Type_ID`)
 SELECT DISTINCT `digest`, `data`, `size`, `compression`, (SELECT ID FROM modul_141.type where type.name = import.type)
-FROM import;
+FROM Import;
 
 INSERT INTO Modul_141.meta(`path`, `perm`, `time`, `Systemuser_UID`, `USergroup_GID`, `Data_digest`)
 SELECT DISTINCT `path`, `perm`, `time`, `uid`, `gid`, `digest`
-FROM import;
+FROM Import;
 
-DROP TABLE import;+
+DROP TABLE Import;+
