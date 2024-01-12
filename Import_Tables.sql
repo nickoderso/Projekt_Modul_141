2b364f25a4c7dbb9bec46eb3a48202e8e7bc0838
Mike <111377817+Chaeschtli@users.noreply.github.com>
2024-01-12 11:03:20 +0100

    Update Import_Tables.sql
    Sollte nun funktionieren

--- Import_Tables.sql
+++ Import_Tables.sql
@@ -1,21 +1,21 @@
 USE Modul_141;
-INSERT INTO Modul_141.type(name) 
+INSERT INTO Modul_141.Type(name) 
 SELECT DISTINCT type
 FROM Import;
 
-INSERT INTO Modul_141.usergroup(`GID`, `Group`)
+INSERT INTO Modul_141.Usergroup(`GID`, `Group`)
 SELECT DISTINCT gid, `group`
 FROM Import;
 
-INSERT INTO Modul_141.systemuser(`UID`, `User`)
+INSERT INTO Modul_141.Systemuser(`UID`, `User`)
 SELECT DISTINCT uid, `user`
 FROM Import;
 
-INSERT INTO Modul_141.data(`digest`, `content`, `size`, `compression`, `Type_ID`)
-SELECT DISTINCT `digest`, `data`, `size`, `compression`, (SELECT ID FROM modul_141.type where type.name = import.type)
+INSERT INTO Modul_141.Data(`digest`, `content`, `size`, `compression`, `Type_ID`)
+SELECT DISTINCT `digest`, `data`, `size`, `compression`, (SELECT ID FROM Modul_141.Type where Type.Name = Import.Type)
 FROM Import;
 
-INSERT INTO Modul_141.meta(`path`, `perm`, `time`, `Systemuser_UID`, `USergroup_GID`, `Data_digest`)
+INSERT INTO Modul_141.Meta(`path`, `perm`, `time`, `Systemuser_UID`, `USergroup_GID`, `Data_digest`)
 SELECT DISTINCT `path`, `perm`, `time`, `uid`, `gid`, `digest`
 FROM Import;
 
